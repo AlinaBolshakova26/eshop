e9ce8c206085b8e22c2427be987e69025831ed9a
Алиса Римша <alice_rimsha@mail.ru>
2025-02-07 16:35:38 +0000

    Убраны лишние данные

--- src/config/database.php
+++ src/config/database.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'DB_HOST' => 'MySQL-8.2',
-    'DB_NAME' => 'bitflix',
-    'DB_USER' => 'student',
-    'DB_PASSWORD' => 'password',
+    'DB_HOST' => '',
+    'DB_NAME' => '',
+    'DB_USER' => '',
+    'DB_PASSWORD' => '',
     'charset' => 'utf8mb4'
 ];
