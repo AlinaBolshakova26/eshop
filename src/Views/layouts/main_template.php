c580fa48b134c1c4857bde35dc86644b108812c9
Alice Rimsha <alice_rimsha@mail.ru>
2025-02-09 18:00:23 +0300

    Исправлена переадресация

--- src/Views/layouts/main_template.php
+++ src/Views/layouts/main_template.php
@@ -24,7 +24,7 @@
 					<a class="nav-link" href="/">Главная</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="/products">Продукты</a>
+					<a class="nav-link" href="/">Продукты</a>
 				</li>
 			</ul>
 		</div>
