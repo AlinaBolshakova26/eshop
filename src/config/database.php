b9abcf343dc5ce5522ba4bf8a72d25db0b72acfd
Алиса Римша <alice_rimsha@mail.ru>
2025-02-10 12:54:10 +0000

    Deleted additional data

--- src/config/database.php
+++ src/config/database.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'DB_HOST' => '127.127.126.50',
-    'DB_NAME' => 'flower_shop',
-    'DB_USER' => 'root',
+    'DB_HOST' => '',
+    'DB_NAME' => '',
+    'DB_USER' => '',
     'DB_PASSWORD' => '',
     'charset' => 'utf8mb4'
 ];
