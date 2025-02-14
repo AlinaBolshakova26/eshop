570f7d65220747bb27bef464ac975847c335d7c2
Alice Rimsha <alice_rimsha@mail.ru>
2025-02-14 13:20:29 +0300

    Improved registration form

--- src/Requests/UserRegistrationRequest.php
+++ src/Requests/UserRegistrationRequest.php
@@ -22,18 +22,34 @@ class UserRegistrationRequest
 
     public function validate(): void
     {
-        if (!$this->name || !$this->phone || !$this->email || !$this->password) {
+        if (!$this->name || !$this->phone || !$this->email || !$this->password)
+        {
             throw new \InvalidArgumentException("Все поля обязательны для заполнения.");
         }
-        if ($this->password !== $this->passwordConfirm) {
+
+        if (!preg_match('/^[A-ZА-Я][a-zа-я]+\s[A-ZА-Я][a-zа-я]+$/u', $this->name))
+        {
+            throw new \InvalidArgumentException("Имя должно содержать минимум два слова, начинающихся с заглавной буквы.");
+        }
+
+        if ($this->password !== $this->passwordConfirm)
+        {
             throw new \InvalidArgumentException("Пароли не совпадают.");
         }
-        if (!filter_var($this->email, FILTER_VALIDATE_EMAIL)) {
+
+        if (strlen($this->password) < 8)
+        {
+            throw new \InvalidArgumentException("Пароль должен содержать минимум 8 символов.");
+        }
+
+        if (!filter_var($this->email, FILTER_VALIDATE_EMAIL))
+        {
             throw new \InvalidArgumentException("Неверный формат email.");
         }
 
-        if (!preg_match('/^\+?[0-9]{10,15}$/', $this->phone)) {
-            throw new \InvalidArgumentException("Некорректный формат телефона. Пример: +79991234567.");
+        if (!preg_match('/^(\+7|8)\d{10}$/', $this->phone))
+        {
+            throw new \InvalidArgumentException("Некорректный формат телефона. Используйте +7 или 8 и далее 10 цифр.");
         }
     }
 
