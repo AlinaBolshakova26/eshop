c7374e5dad95f9e93caaa078587d5ea4f1727279
Alexandr Trofimenko <trofimenko02@bk.ru>
2025-02-17 00:59:51 +0200

    Исправление путей для фильтрации

--- src/config/routes.php
+++ src/config/routes.php
@@ -6,9 +6,8 @@ $router = new Router();
 
 
 $router->addRoute('GET', '/', [\Controllers\HomeController::class, 'index']);
-$router->addRoute('GET', '/tag/', [\Controllers\HomeController::class, 'index']);
-$router->addRoute('GET', '/tag/{id:\d+}', [\Controllers\HomeController::class, 'index'])
-;$router->addRoute('GET', '/search', [\Controllers\HomeController::class, 'index']);
+$router->addRoute('GET', '/tag', [\Controllers\HomeController::class, 'index']);$router->addRoute('GET', '/tag/{id:\d+}', [\Controllers\HomeController::class, 'index']);
+$router->addRoute('GET', '/search', [\Controllers\HomeController::class, 'index']);
 $router->addRoute('GET', '/search/{query:[^/]+}', [\Controllers\HomeController::class, 'index']);
 
 
