0050fe3747d54ec2f6cc11274b0824fbef367038
«Алина <alinabolshakova26@gmail.com>
2025-02-18 15:56:54 +0200

    change migration

--- sql/migrations/14_02_2025_create_table_cart.sql
+++ sql/migrations/14_02_2025_create_table_cart.sql
@@ -4,6 +4,7 @@ CREATE TABLE up_cart (
                          item_id INT NOT NULL,
                          quantity INT NOT NULL DEFAULT 1,
                          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                          FOREIGN KEY (user_id) REFERENCES up_user(id) ON DELETE CASCADE,
                          FOREIGN KEY (item_id) REFERENCES up_item(id) ON DELETE CASCADE
 )
